c303679e5392b114480904ea76438fd92d39c80c
Prachi Kumari <prachikumari1027@gmail.com>
2025-07-08 13:43:10 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+       fontFamily: {
+        boldonse: ['"Boldonse"', 'system-ui','Lora'],
+      },
+    },
+  },
+  plugins: [],
+}
+
